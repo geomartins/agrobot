086e47a887962edcfa3e11b8b9f94fbcb353d218
Martins Abiodun <martinsabiodun94@gmail.com>
2019-08-07 09:13:28 +0100

    checking if the cors works

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import VueApexCharts from 'vue-apexcharts'
 import VeeValidate from 'vee-validate'
 import Connection from './views/layouts/Connection'
 
+axios.defaults.withCredentials = true;
+
 
 
 
