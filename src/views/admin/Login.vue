569a4ac5ac37b4e3fb688387183749a000aaed7f
Martins Abiodun <martinsabiodun94@gmail.com>
2019-08-07 12:03:29 +0100

    tetst2

--- src/views/admin/Login.vue
+++ src/views/admin/Login.vue
@@ -123,7 +123,8 @@ export default {
                  await this.api_calls2(api_payload).then(()=>{
 
                     setTimeout(function(){ 
-                      x.$router.push('/admin/dashboard');
+                      // x.$router.push('/admin/dashboard');
+                      window.location.href="/admin/dashboard";
                     }, 3000);
                      
                  })
