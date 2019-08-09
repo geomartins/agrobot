1d24bd170ca8d1ee8e09474a803601731275f071
Martins Abiodun <martinsabiodun94@gmail.com>
2019-08-09 22:17:52 +0100

    still testing

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
+
+    
     // options...
     devServer: {
-          proxy: 'https://lumen.lilycourt.ng/',
-      }
+        proxy: {
+            '^/api': {
+              target: 'https://lumen.lilycourt.ng/',
+              ws: true,
+              changeOrigin: true
+            },
+            
+        }
+         // proxy: 'https://lumen.lilycourt.ng/',
+    }
 }
