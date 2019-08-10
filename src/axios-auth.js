04de647544cd0cd4128ef6b130eccabdce23b5cf
Martins Abiodun <martinsabiodun94@gmail.com>
2019-08-11 00:35:55 +0100

    still testing

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://agrobot.onrender.com'
 const instance = axios.create({
-    baseURL: 'https://agrobot.onrender.com'
+    baseURL: 'https://lumen.lilycourt.ng'
 })
 instance.defaults.baseURL = 'https://lumen.lilycourt.ng'
 export default instance
