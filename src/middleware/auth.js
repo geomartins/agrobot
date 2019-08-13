d8d6ea6bd21bb1ecd618e65a467b771d8ba41c22
Martins Abiodun <martinsabiodun94@gmail.com>
2019-08-13 16:21:29 +0100

    adding auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,23 +5,23 @@ export const auth ={
 
     methods:{
 
-        protectAdmin(){
+        // protectAdmin(){
 
-            let response = this.$store.dispatch('protectAdmin',{ token: this.$store.getters.getToken});
-            if(response && 'status' in response){
-                if(response.status == 404){
+        //     let response = this.$store.dispatch('protectAdmin',{ token: this.$store.getters.getToken});
+        //     if(response && 'status' in response){
+        //         if(response.status == 404){
 
-                    console.log(response.data.message)
-                    this.customLogout();
-                }
+        //             console.log(response.data.message)
+        //             this.customLogout();
+        //         }
 
-                if(response.status == 200){
-                    console.log(response.data.message)
-                }
-            }
+        //         if(response.status == 200){
+        //             console.log(response.data.message)
+        //         }
+        //     }
 
             
-        },
+        // },
 
         
     }
--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -224,18 +224,7 @@
       
       async created(){
 
-        let response = await this.$store.dispatch('protectAdmin',{ token: this.$store.getters.getToken});
-            if(response && 'status' in response){
-                if(response.status == 404){
-
-                    console.log(response.data.message)
-                    //this.customLogout();
-                }
-
-                if(response.status == 200){
-                    console.log(response.data.message)
-                }
-        }
+        this.protectAdmin();
 
 
           
