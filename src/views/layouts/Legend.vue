0b3b1b9a0bf10a39acfb69e989f4a7f618e1e868
Martins Abiodun <martinsabiodun94@gmail.com>
2019-08-13 19:07:14 +0100

    adding auth middleware

--- src/views/layouts/Legend.vue
+++ src/views/layouts/Legend.vue
@@ -42,14 +42,12 @@ export default {
     let response = await this.$store.dispatch('protectAdmin',{ token: this.$store.getters.getToken});
     if(response && 'status' in response){
         if(response.status == 404){
-
-            console.log(response.data.message)
-            this.customLogout();
+            if(response.data.message.includes('Unauthenticated')){
+                 this.customLogout();
+            }
+           
         }
 
-        if(response.status == 200){
-            console.log(response.data.message)
-        }
     }
   }
   
